db9eb2c5e7c3e632dd02b751cf5f938270f7bf7e
albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
2023-02-03 11:51:11 +0100

    ISI-428 MasterEaiApiRequestMixin implementiert

---
+++ frontend/src/mixins/requests/eai/MasterEaiApiRequestMixin.ts
@@ -0,0 +1,36 @@
+import {Component, Mixins} from "vue-property-decorator";
+import {
+  MasterEaiApi,
+  GetAdressenRequest,
+  AdresseDto,
+  MuenchenAdresseDto
+} from "@/api/api-client/isi-master-eai";
+import RequestUtils from "@/utils/RequestUtils";
+import ErrorHandler from "@/mixins/requests/ErrorHandler";
+
+@Component
+export default class MasterEaiApiRequestMixin extends Mixins(
+  ErrorHandler
+) {
+  
+  private masterEaiApi: MasterEaiApi;
+  
+  constructor() {
+    super();
+    this.masterEaiApi = new MasterEaiApi(RequestUtils.getBasicFetchConfigurationForMasterEai());
+  }
+  
+  getInfrastrukturabfragen(dto: AdresseDto, showInInformationList: boolean): Promise<MuenchenAdresseDto[]> {
+    const requestObject: GetAdressenRequest = {
+      adresseDto: dto
+    };
+    return this.masterEaiApi.getAdressen(requestObject, RequestUtils.getPOSTConfig())
+      .then(response => {
+        return response;
+      })
+      .catch(error => {
+        throw this.handleError(showInInformationList, error);
+      });
+  }
+  
+}
