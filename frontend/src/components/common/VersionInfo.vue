59989b66ab770f0e301d54a992459794f979818c
banzuu <103417158+banzuu@users.noreply.github.com>
2023-02-13 14:01:33 +0100

    Kommentar als Blockkommentar

--- frontend/src/components/common/VersionInfo.vue
+++ frontend/src/components/common/VersionInfo.vue
@@ -133,8 +133,11 @@ export default class VersionInfo extends Vue {
       }
 
       this.services = services;
-      // Ohne diese Abfrage könnte fetchSuccess=true gesetzt werden, obwohl die Request vorher fehlgeschlagen ist.
-      // Es wird ein strikter Vergleich mit false verwendet, da der Wert auch null sein könnte.
+      
+      /*
+      Ohne diese Abfrage könnte fetchSuccess=true gesetzt werden, obwohl die Request vorher fehlgeschlagen ist.
+      Es wird ein strikter Vergleich mit false verwendet, da der Wert auch null sein könnte.
+      */
       if (this.fetchSuccess !== false) {
         this.fetchSuccess = true;
       }
