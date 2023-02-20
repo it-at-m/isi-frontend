49c337955966156b956b71e25247118ebf128420
banzuu <103417158+banzuu@users.noreply.github.com>
2023-02-20 12:53:24 +0100

    WFS-API integriert

---
+++ frontend/src/api/api-client/isi-wfs-eai/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ControllerFrPolygonKoordinatenImStandardEPSG4326WGS84Api';
+export * from './ControllerFrPolygonspezifischeOperationenImStandardEPSG4326WGS84Api';
+export * from './ControllerFrPunktKoordinatenImStandardEPSG4326WGS84Api';
