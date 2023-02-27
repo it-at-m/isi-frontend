027b2b483449eb290cbc6531cfe08e95aa676f3a
banzuu <103417158+banzuu@users.noreply.github.com>
2023-02-27 11:27:32 +0100

    Update WFS API

--- frontend/src/api/api-client/isi-wfs-eai/apis/index.ts
+++ frontend/src/api/api-client/isi-wfs-eai/apis/index.ts
@@ -1,5 +1,5 @@
 /* tslint:disable */
 /* eslint-disable */
-export * from './ControllerFrPolygonKoordinatenImStandardEPSG4326WGS84Api';
-export * from './ControllerFrPolygonspezifischeOperationenImStandardEPSG4326WGS84Api';
-export * from './ControllerFrPunktKoordinatenImStandardEPSG4326WGS84Api';
+export * from './PolygonApi';
+export * from './PolygonOperationenApi';
+export * from './PunktApi';
--- frontend/src/mixins/requests/eai/WfsEaiApiRequestMixin.ts
+++ frontend/src/mixins/requests/eai/WfsEaiApiRequestMixin.ts
@@ -1,9 +1,12 @@
 import {Component, Mixins} from "vue-property-decorator";
 import {
-  ControllerFrPunktKoordinatenImStandardEPSG4326WGS84Api,
+  PunktApi,
   GetFlurstuecke1Request,
   FlurstueckFeatureDto,
   CoordinateDto,
+  PolygonOperationenApi,
+  UnifyRequest,
+  CoordinatesDto,
 } from "@/api/api-client/isi-wfs-eai";
 import RequestUtils from "@/utils/RequestUtils";
 import ErrorHandler from "@/mixins/requests/ErrorHandler";
@@ -14,21 +17,40 @@ export default class WfsEaiApiRequestMixin extends Mixins(
   ErrorHandler
 ) {
   
-  private wfsEaiApi: ControllerFrPunktKoordinatenImStandardEPSG4326WGS84Api;
+  private punktApi: PunktApi;
+  private operationApi: PolygonOperationenApi;
   
   constructor() {
     super();
-    this.wfsEaiApi = new ControllerFrPunktKoordinatenImStandardEPSG4326WGS84Api(RequestUtils.getBasicFetchConfigurationForWfsEai());
+    this.punktApi = new PunktApi(RequestUtils.getBasicFetchConfigurationForWfsEai());
+    this.operationApi = new PolygonOperationenApi(RequestUtils.getBasicFetchConfigurationForWfsEai());
   }
 
-  async getFlurstuecke(coordinateDto: CoordinateDto, showInInformationList: boolean): Promise<FlurstueckFeatureDto[]> {
-    const request: GetFlurstuecke1Request = { coordinateDto };
+  async getFlurstuecke(coordinate: CoordinateDto, showInInformationList: boolean): Promise<FlurstueckFeatureDto[]> {
+    const request: GetFlurstuecke1Request = { coordinateDto: coordinate };
     let flurstuecke: FlurstueckFeatureDto[] = [];
 
-    await this.wfsEaiApi.getFlurstuecke1(request)
+    await this.punktApi.getFlurstuecke1(request)
       .then(response => {
         if (!_.isNil(response.flurstuecke)) {
           flurstuecke = response.flurstuecke;
+        }
+      })
+      .catch(error => {
+        throw this.handleError(showInInformationList, error);
+      });
+    
+    return flurstuecke;
+  }
+
+  async getUnion(coordinates: CoordinatesDto[], showInInformationList: boolean): Promise<CoordinatesDto[]> {
+    const request: UnifyRequest = { polygonsDto: { polygons: coordinates } };
+    let flurstuecke: CoordinatesDto[] = [];
+
+    await this.operationApi.unify(request)
+      .then(response => {
+        if (!_.isNil(response.polygons)) {
+          flurstuecke = response.polygons;
         }
       })
       .catch(error => {
