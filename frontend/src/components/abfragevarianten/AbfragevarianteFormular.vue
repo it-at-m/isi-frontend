5769f601ce26bd28dd2581c02067dac3249f0063
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-03-14 16:34:36 +0100

    change headline abfragevariante

--- frontend/src/components/abfragevarianten/AbfragevarianteFormular.vue
+++ frontend/src/components/abfragevarianten/AbfragevarianteFormular.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row>
-      <v-col 
+      <v-col
         cols="12"
         md="12"
       >
@@ -253,8 +253,8 @@
 </template>
 
 <script lang="ts">
-import { Component, Mixins, VModel, Prop, Watch } from "vue-property-decorator";
-import { AbfragevarianteDtoPlanungsrechtEnum, LookupEntryDto, UncertainBoolean } from "@/api/api-client/isi-backend";
+import {Component, Mixins, Prop, VModel, Watch} from "vue-property-decorator";
+import {AbfragevarianteDtoPlanungsrechtEnum, LookupEntryDto, UncertainBoolean} from "@/api/api-client/isi-backend";
 import AbfragevarianteModel from "@/types/model/abfragevariante/AbfragevarianteModel";
 import FieldValidationRulesMixin from "@/mixins/validation/FieldValidationRulesMixin";
 import FieldPrefixesSuffixes from "@/mixins/FieldPrefixesSuffixes";
@@ -263,13 +263,13 @@ import NumField from "@/components/common/NumField.vue";
 import SaveLeaveMixin from "@/mixins/SaveLeaveMixin";
 import DisplayMode from "@/types/common/DisplayMode";
 
-@Component({ components: { FieldGroupCard, NumField } })
+@Component({components: {FieldGroupCard, NumField}})
 export default class AbfragevarianteForm extends Mixins(
-  FieldPrefixesSuffixes,
-  FieldValidationRulesMixin,
-  SaveLeaveMixin,
+    FieldPrefixesSuffixes,
+    FieldValidationRulesMixin,
+    SaveLeaveMixin,
 ) {
-  @VModel({ type: AbfragevarianteModel }) abfragevariante!: AbfragevarianteModel;
+  @VModel({type: AbfragevarianteModel}) abfragevariante!: AbfragevarianteModel;
 
   @Prop()
   private mode!: DisplayMode;
@@ -293,10 +293,10 @@ export default class AbfragevarianteForm extends Mixins(
     return this.sobonRelevant;
   }
 
-  get isGeschossflaecheSobonUrsaechlichPflichtfeld(): boolean {  
+  get isGeschossflaecheSobonUrsaechlichPflichtfeld(): boolean {
     const pflichtfeld = this.isSobonRelevant === UncertainBoolean.True
-     && (this.abfragevariante.planungsrecht === AbfragevarianteDtoPlanungsrechtEnum.BplanParag12
-      || this.abfragevariante.planungsrecht === AbfragevarianteDtoPlanungsrechtEnum.BplanParag11);
+        && (this.abfragevariante.planungsrecht === AbfragevarianteDtoPlanungsrechtEnum.BplanParag12
+            || this.abfragevariante.planungsrecht === AbfragevarianteDtoPlanungsrechtEnum.BplanParag11);
     this.componentKeyGeschossflaecheSobonUrsaechlich++; // Trigger, damit die Komponente neu gerendert wird
     return pflichtfeld;
   }
@@ -310,10 +310,11 @@ export default class AbfragevarianteForm extends Mixins(
   }
 
   get headline(): string {
-    return this.displayMode === DisplayMode.NEU ? "Abfragevariante anlegen" : `Abfragevariante Nr. ${this.abfragevariante.abfragevariantenNr} ändern`;
+    const headline = `Abfragevariante Nr. ${this.abfragevariante.abfragevariantenNr} `;
+    return this.displayMode === DisplayMode.NEU ? headline.concat("anlegen") : headline.concat("ändern");
   }
 
-  @Watch("abfragevariante", { immediate: true, deep: true })
+  @Watch("abfragevariante", {immediate: true, deep: true})
   public clearSonderwohnformData(): void {
     if (!this.abfragevariante.sonderwohnformen) {
       this.abfragevariante.geschossflaecheStudentenwohnungen = undefined;
