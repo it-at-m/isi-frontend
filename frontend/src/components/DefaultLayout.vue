5bdb84b3a755a7b2d35ce81e1ca96935b76e7426
TiagoKocevar <tiago.kocevar@gmail.com>
2023-03-29 16:00:02 +0200

    🚧 Testing v-list-item-action and 40 %

--- frontend/src/components/DefaultLayout.vue
+++ frontend/src/components/DefaultLayout.vue
@@ -123,7 +123,7 @@ export default class DefaultLayout extends Vue {
 
 .side-bar-information {
   width: 100%;
-  height: 65%;
+  height: 60%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -131,7 +131,7 @@ export default class DefaultLayout extends Vue {
 
 .side-bar-action {
   width: 100%;
-  height: 35%;
+  height: 40%;
   display: flex;
   flex-direction: column;
   align-items: center;
--- frontend/src/views/Abfrage.vue
+++ frontend/src/views/Abfrage.vue
@@ -107,21 +107,23 @@
       </template>
       <template #action>
         <v-spacer />
-        <div
+        <v-list-item
           v-for="(transition, index) in possbileTransitions"
           :key="index"
         >
-          <v-btn
-            v-if="!isNewAbfrage()"
-            :id="'abfrage_status_aenderung' + index + '_button'"
-            color="secondary"
-            class="text-wrap mt-2 px-1"
-            elevation="1"
-            style="width: 200px"
-            @click="statusUebergang(transition)"
-            v-text="transition.buttonName"
-          />
-        </div>
+          <v-list-item-action>
+            <v-btn
+              v-if="!isNewAbfrage()"
+              :id="'abfrage_status_aenderung' + index + '_button'"
+              color="secondary"
+              class="text-wrap mt-2 px-1"
+              elevation="1"
+              style="width: 200px"
+              @click="statusUebergang(transition)"
+              v-text="transition.buttonName"
+            />
+          </v-list-item-action>
+        </v-list-item>
         <v-btn
           id="abfrage_speichern_button"
           class="text-wrap mt-2 px-1"
