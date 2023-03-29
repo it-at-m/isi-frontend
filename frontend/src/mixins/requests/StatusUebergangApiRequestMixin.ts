724024d8a05a36b121c16a2c66d0bdab94780428
banzuu <103417158+banzuu@users.noreply.github.com>
2023-03-29 11:14:24 +0200

    Formatierungen

--- frontend/src/mixins/requests/StatusUebergangApiRequestMixin.ts
+++ frontend/src/mixins/requests/StatusUebergangApiRequestMixin.ts
@@ -6,22 +6,21 @@ import { Levels } from "@/api/error";
 
 @Component
 export default class StatusUebergangApiRequestMixin extends Vue {
-
   async statusUebergangRequest(transition: TransitionDto, abfrageId: string | undefined): Promise<boolean> {
     let successFullRequest = false;
     const fetchUrl =
       import.meta.env.VITE_VUE_APP_API_URL +
       `/api/isi-backend-service//infrastruktur-abfrage/${abfrageId}/${transition.url}`;
     await fetch(fetchUrl, RequestUtils.getPUTConfig())
-    .then((response) => {
-      if(response.status == 200) {
-        successFullRequest = true;
-      }
-    })
-    .catch(() => {
-      Toaster.toast(`Beim ${transition.buttonName} ist ein Fehler aufgetreten`, Levels.ERROR);
-      successFullRequest = false;
-    });
+      .then((response) => {
+        if (response.status == 200) {
+          successFullRequest = true;
+        }
+      })
+      .catch(() => {
+        Toaster.toast(`Beim ${transition.buttonName} ist ein Fehler aufgetreten`, Levels.ERROR);
+        successFullRequest = false;
+      });
     return successFullRequest;
   }
 }
