8a7f77beace0fae4343f40a819ed7f4aaee095b1
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-07-28 14:11:19 +0200

    add speed dial

--- frontend/src/views/Karte.vue
+++ frontend/src/views/Karte.vue
@@ -6,6 +6,56 @@
     <template #content>
       <city-map />
     </template>
+    <template #action>
+      <v-spacer />
+      <div class="align-self-end">
+        <v-speed-dial
+          v-model="speedDialOpen"
+          bottom
+          right
+        >
+          <template #activator>
+            <v-btn
+              v-model="speedDialOpen"
+              color="secondary"
+              dark
+              fab
+              x-large
+            >
+              <v-icon v-if="speedDialOpen"> mdi-close </v-icon>
+              <v-icon v-else> mdi-plus </v-icon>
+            </v-btn>
+          </template>
+          <v-btn
+            class="text-h6"
+            fab
+            dark
+            large
+            color="red"
+            @click="createInfrastruktureinrichtung"
+            v-text="'I'"
+          />
+          <v-btn
+            class="text-h6"
+            fab
+            dark
+            large
+            color="indigo"
+            @click="createBauvorhaben"
+            v-text="'B'"
+          />
+          <v-btn
+            class="text-h6"
+            fab
+            dark
+            large
+            color="green"
+            @click="createAbfrage"
+            v-text="'A'"
+          />
+        </v-speed-dial>
+      </div>
+    </template>
   </default-layout>
 </template>
 
@@ -14,6 +64,7 @@ import { Vue, Component } from "vue-property-decorator";
 import DefaultLayout from "@/components/DefaultLayout.vue";
 import SearchResultList from "@/components/search/SearchResultList.vue";
 import CityMap from "@/components/map/CityMap.vue";
+import router from "@/router";
 
 @Component({
   components: {
@@ -22,5 +73,25 @@ import CityMap from "@/components/map/CityMap.vue";
     DefaultLayout,
   },
 })
-export default class Karte extends Vue {}
+export default class Karte extends Vue {
+  private speedDialOpen = false;
+
+  private createAbfrage(): void {
+    router.push({
+      name: "newabfrage",
+    });
+  }
+
+  private createBauvorhaben(): void {
+    router.push({
+      name: "createBauvorhaben",
+    });
+  }
+
+  private createInfrastruktureinrichtung(): void {
+    router.push({
+      name: "createInfrastruktureinrichtung",
+    });
+  }
+}
 </script>
