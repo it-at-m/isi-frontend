f9f083a69ce093f189d4c2f515673a95a8aa76bd
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-07-31 13:12:42 +0200

    set kaptiel abfragevariante

--- frontend/src/types/model/abfragevariante/AbfragevarianteModel.ts
+++ frontend/src/types/model/abfragevariante/AbfragevarianteModel.ts
@@ -1,10 +1,23 @@
 import { AbfragevarianteDto } from "@/api/api-client/isi-backend";
+import { AnzeigeContextAbfragevariante } from "@/views/Abfrage.vue";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface AbfragevarianteModel extends AbfragevarianteDto {}
 class AbfragevarianteModel {
   constructor(abfragevariante: AbfragevarianteDto) {
     Object.assign(this, abfragevariante, {});
+  }
+
+  public getAbfragevariantenNrForContextAnzeigeAbfragevariante(
+    contextAnzeigeAbfragevariante: AnzeigeContextAbfragevariante
+  ): string | undefined {
+    let numberContext: string | undefined = undefined;
+    if (contextAnzeigeAbfragevariante === AnzeigeContextAbfragevariante.ABFRAGEVARIANTE) {
+      numberContext = `1.${this.abfragevariantenNr}`;
+    } else if (contextAnzeigeAbfragevariante === AnzeigeContextAbfragevariante.ABFRAGEVARIANTE_SACHBEARBEITUNG) {
+      numberContext = `2.${this.abfragevariantenNr}`;
+    }
+    return numberContext;
   }
 }
 export { AbfragevarianteModel as default };
