f43fcd4c920c39e419d1775cc1bd48cee3206727
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2023-09-08 11:10:24 +0200

    add api docs

--- frontend/src/api/api-client/isi-backend/apis/SucheApi.ts
+++ frontend/src/api/api-client/isi-backend/apis/SucheApi.ts
@@ -15,6 +15,9 @@
 
 import * as runtime from '../runtime';
 import {
+    InformationResponseDto,
+    InformationResponseDtoFromJSON,
+    InformationResponseDtoToJSON,
     SearchQueryAndSortingDto,
     SearchQueryAndSortingDtoFromJSON,
     SearchQueryAndSortingDtoToJSON,
