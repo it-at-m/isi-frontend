56954d84a058ae91dcf73aac47359effb0d91cb2
lhmAschaenz <119875041+lhmAschaenz@users.noreply.github.com>
2023-09-28 16:11:37 +0200

    ISI-156-Abfragevariante-Informationen-zu-Baugebieten (#88)
    * ISI-156 Basic version
    
    * ISI-156 Horiz. Auflistung der Bauraten, Bugfixes
    
    * ISI-156 Umstellung auf ein Property
    
    * ISI-156 Recursive aggregation
    
    * ISI-156 Bugfix Typo
    
    * ISI-156 Review Anmerkungen eingearbeitet
    
    * ISI-156 Code Optimierung
    
    * ISI-156 Leerzeile gelöscht
    
    * fix bug
    
    * create model of bauraten
    
    * fix bug
    
    * set up model classes
    
    * formation of bauraten
    
    * change table
    
    * change titel
    
    * ISI-156 Merge 'dev' into branch
    
    * ISI-156 Bugfixes aus Test
    
    * ISI-156 Lint formatiert
    
    ---------
    
    Co-authored-by: albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
    Co-authored-by: alexander.boxhorn <alexander.boxhorn@muenchen.de>

--- frontend/src/types/model/bauabschnitte/BauabschnittModel.ts
+++ frontend/src/types/model/bauabschnitte/BauabschnittModel.ts
@@ -1,10 +1,13 @@
 import { BauabschnittDto } from "@/api/api-client/isi-backend";
+import _ from "lodash";
+import BaugebietModel from "@/types/model/baugebiete/BaugebietModel";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface BauabschnittModel extends BauabschnittDto {}
 class BauabschnittModel {
   constructor(bauabschnitt: BauabschnittDto) {
     Object.assign(this, bauabschnitt, {});
+    this.baugebiete = _.toArray(bauabschnitt.baugebiete).map((baugebiet) => new BaugebietModel(baugebiet));
   }
 }
 export { BauabschnittModel as default };
--- frontend/src/types/model/baugebiete/BaugebietModel.ts
+++ frontend/src/types/model/baugebiete/BaugebietModel.ts
@@ -1,10 +1,13 @@
 import { BaugebietDto } from "@/api/api-client/isi-backend";
+import _ from "lodash";
+import BaurateModel from "@/types/model/bauraten/BaurateModel";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface BaugebietModel extends BaugebietDto {}
 class BaugebietModel {
   constructor(baugebiet: BaugebietDto) {
     Object.assign(this, baugebiet, {});
+    this.bauraten = _.toArray(baugebiet.bauraten).map((baurate) => new BaurateModel(baurate));
   }
 }
 export { BaugebietModel as default };
