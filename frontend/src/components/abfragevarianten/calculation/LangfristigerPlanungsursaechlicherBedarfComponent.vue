a179e504fbc01aeaf74c576622a9cfcb2884f27e
boal <56300801+boal@users.noreply.github.com>
2023-11-30 11:49:22 +0100

    ISI-928 ISI-88 Darstellung der planungsursächlichen Bedarf (#150)
    * add table bedarf infrastrukturbedarf
    
    * add expansion panel
    
    * adapt design
    
    * add expansion panel
    
    * change format of mittelwert
    
    * change format of mittelwert
    
    * round to fixed decimals
    
    * change attributes
    
    * add alle einwohner component
    
    * adapt request
    
    * adapt components for bedarfe

--- frontend/src/components/abfragevarianten/calculation/LangfristigerPlanungsursaechlicherBedarfComponent.vue
+++ frontend/src/components/abfragevarianten/calculation/LangfristigerPlanungsursaechlicherBedarfComponent.vue
@@ -4,15 +4,27 @@
       title="Datengrundlage für Bedarfsbestimmung"
       :items="wohneinheiten"
     />
+    <infrastrukturbedarf-component
+      :infrastruktur-bedarfe-pro-jahr="bedarfKinderkrippe"
+      :title="'Kinderkrippe'"
+    />
+    <infrastrukturbedarf-component
+      :infrastruktur-bedarfe-pro-jahr="bedarfKindergarten"
+      :title="'Kindergarten'"
+    />
+    <alle-einwohner-component :personen-pro-jahr="alleEinwohner" />
   </field-group-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
 import FieldGroupCard from "@/components/common/FieldGroupCard.vue";
+
 import WohneinheitenComponent from "@/components/abfragevarianten/calculation/WohneinheitenComponent.vue";
 import {
+  InfrastrukturbedarfProJahrDto,
   LangfristigerPlanungsursaechlicherBedarfDto,
+  PersonenProJahrDto,
   WohneinheitenProFoerderartProJahrDto,
 } from "@/api/api-client/isi-backend";
 
@@ -29,6 +41,42 @@ export default class LangfristigerPlanungsursaechlicherBedarfComponent extends V
         ...this.bedarf.wohneinheitenSumme15Jahre,
         ...this.bedarf.wohneinheitenSumme20Jahre,
         ...this.bedarf.wohneinheitenGesamt,
+      ];
+    }
+    return [];
+  }
+
+  get bedarfKinderkrippe(): Array<InfrastrukturbedarfProJahrDto> | undefined {
+    if (this.bedarf) {
+      return [
+        ...this.bedarf.bedarfKinderkrippe,
+        this.bedarf.bedarfKinderkrippeMittelwert10,
+        this.bedarf.bedarfKinderkrippeMittelwert15,
+        this.bedarf.bedarfKinderkrippeMittelwert20,
+      ];
+    }
+    return [];
+  }
+
+  get bedarfKindergarten(): Array<InfrastrukturbedarfProJahrDto> | undefined {
+    if (this.bedarf) {
+      return [
+        ...this.bedarf.bedarfKindergarten,
+        this.bedarf.bedarfKindergartenMittelwert10,
+        this.bedarf.bedarfKindergartenMittelwert15,
+        this.bedarf.bedarfKindergartenMittelwert20,
+      ];
+    }
+    return [];
+  }
+
+  get alleEinwohner(): Array<PersonenProJahrDto> | undefined {
+    if (this.bedarf) {
+      return [
+        ...this.bedarf.alleEinwohner,
+        this.bedarf.alleEinwohnerMittelwert10,
+        this.bedarf.alleEinwohnerMittelwert15,
+        this.bedarf.alleEinwohnerMittelwert20,
       ];
     }
     return [];
