0bd9fcc06a92668514bb6bf63b2fe7040ae3b313
Dominik Grenz <dominik.grenz@muenchen.de>
2023-12-01 09:53:02 +0100

    generate folder new

---
+++ frontend/src/api/api-client/isi-backend/apis/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AbfrageStatusApi';
+export * from './AbfragenApi';
+export * from './BaurateApi';
+export * from './BauvorhabenApi';
+export * from './DateihandlingApi';
+export * from './DokumenteApi';
+export * from './FileInfoStammApi';
+export * from './FoerdermixStammApi';
+export * from './InfrastruktureinrichtungApi';
+export * from './KommentareApi';
+export * from './KoordinatenApi';
+export * from './LookupApi';
+export * from './MimeTypeApi';
+export * from './StammdatenApi';
+export * from './SucheApi';
