79118837027cd2bacf345063fcbc35f8740e2a81
Roternando <150023352+Roternando@users.noreply.github.com>
2024-02-19 10:29:49 +0100

    ran prettier

--- frontend/src/components/bauvorhaben/BauvorhabenForm.vue
+++ frontend/src/components/bauvorhaben/BauvorhabenForm.vue
@@ -31,7 +31,8 @@
               v-model="bauvorhaben.standVerfahrenFreieEingabe"
               :disabled="!isEditable"
               label="freie Eingabe"
-              maxlength=""1000
+              maxlength=""
+              1000
               @input="formChanged"
             />
           </v-slide-y-reverse-transition>
