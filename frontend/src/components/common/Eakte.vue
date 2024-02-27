69828fe25b0d697e941d4a8ec515f79141f230d5
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-02-27 14:40:47 +0100

    refactor eakte component

--- frontend/src/components/common/Eakte.vue
+++ frontend/src/components/common/Eakte.vue
@@ -1,58 +1,82 @@
 <template>
   <div>
-    <v-row v-if="isLinkEakteEditable">
-      <v-col cols="10">
+    <v-row>
+      <v-col cols="12">
+        <span
+          v-if="isEditable"
+          class="v-label theme--light"
+          >{{ title }}</span
+        >
+        <span
+          v-else
+          class="v-label grey--text text--lighten-1"
+          >{{ title }}</span
+        >
+      </v-col>
+    </v-row>
+    <v-row v-if="editModeTextFieldLinkEakte">
+      <v-col
+        cols="12"
+        md="11"
+      >
         <v-textarea
           id="e_akte_field"
           ref="eAkteField"
           v-model="editFieldLinkEakte"
-          label="eAkte"
           auto-grow
-          rows="3"
+          rows="1"
           maxlength="8000"
+          :disabled="!isEditable"
           @input="formChanged"
         />
       </v-col>
-      <v-col cols="2">
-        <v-btn
-          id="link_eakte_uebernehmen_button"
-          class="text-wrap"
-          title="Übernehmen"
-          @click="uebernehmenEakte"
-        >
-          <v-icon> mdi-checkbox-marked-outline </v-icon>
-        </v-btn>
+      <v-col
+        cols="12"
+        md="1"
+      >
+        <v-row class="justify-center">
+          <v-btn
+            id="link_eakte_uebernehmen_button"
+            class="mt-3 mb-14"
+            icon
+            :disabled="!isEditable"
+            @click="uebernehmenEakte"
+          >
+            <v-icon>mdi-checkbox-marked</v-icon>
+          </v-btn>
+        </v-row>
       </v-col>
     </v-row>
-    <div>
-      <v-row v-if="!isLinkEakteEditable">
-        <v-col cols="12">
-          <span class="v-label theme--light">eAkte</span>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="10">
-          <a
-            v-show="isLinkEakteVisible"
-            target="_blank"
-            :href="linkEakteFormatted"
-          >
-            {{ linkEakte }}<span class="mdi mdi-launch" />
-          </a>
-        </v-col>
-        <v-col cols="2">
+    <v-row v-if="!editModeTextFieldLinkEakte">
+      <v-col
+        cols="12"
+        md="11"
+      >
+        <a
+          v-show="isLinkEakteEmpty"
+          target="_blank"
+          :href="linkEakteFormatted"
+        >
+          {{ linkEakte }}<span class="mdi mdi-launch" />
+        </a>
+      </v-col>
+      <v-col
+        cols="12"
+        md="1"
+      >
+        <v-row class="justify-center">
           <v-btn
-            v-show="isEditable && !isLinkEakteEditable"
             id="link_eakte_bearbeiten_button"
-            class="text-wrap"
-            title="Bearbeiten"
+            class="mt-3 mb-14"
+            icon
+            :disabled="!isEditable"
             @click="editEakte"
           >
-            <v-icon> mdi-pencil-box-outline </v-icon>
+            <v-icon>mdi-pencil-box</v-icon>
           </v-btn>
-        </v-col>
-      </v-row>
-    </div>
+        </v-row>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -64,12 +88,15 @@ import _ from "lodash";
 
 @Component
 export default class Eakte extends Mixins(FieldValidationRulesMixin, SaveLeaveMixin) {
+  private readonly title = "Link eAkte";
+
   @VModel({ type: String }) linkEakte!: string | undefined;
 
   @Prop({ type: Boolean, default: false })
   private isEditable!: boolean;
 
   private textFieldLinkEakte = "";
+
   private editModeTextFieldLinkEakte = false;
 
   get editFieldLinkEakte(): string {
@@ -79,12 +106,9 @@ export default class Eakte extends Mixins(FieldValidationRulesMixin, SaveLeaveMi
   set editFieldLinkEakte(linkEakte: string) {
     this.textFieldLinkEakte = linkEakte;
   }
-  get isLinkEakteEditable(): boolean {
-    return this.isEditable && this.editModeTextFieldLinkEakte;
-  }
 
-  get isLinkEakteVisible(): boolean {
-    return !_.isEmpty(this.linkEakte) && !this.isLinkEakteEditable;
+  get isLinkEakteEmpty(): boolean {
+    return !_.isEmpty(this.linkEakte);
   }
 
   private uebernehmenEakte(): void {
