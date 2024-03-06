306243f336c8de9d3807e193083d75ca66ab383d
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-06 10:00:12 +0100

    refactor component for benutzerinfosrmation

--- frontend/src/components/common/Benutzerinformationen.vue
+++ frontend/src/components/common/Benutzerinformationen.vue
@@ -20,27 +20,25 @@
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-title>Name:</v-list-item-title>
-          <v-list-item-subtitle>{{ benutzerinformationen.bearbeitendePerson.name }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ name }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-title>Email:</v-list-item-title>
-          <v-list-item-subtitle>{{ benutzerinformationen.bearbeitendePerson.email }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ email }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-title>Organisationseinheit:</v-list-item-title>
-          <v-list-item-subtitle>{{
-            benutzerinformationen.bearbeitendePerson.organisationseinheit
-          }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ organisationseinheit }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-list-item two-line>
         <v-list-item-content>
           <v-list-item-title>Letzte Änderung:</v-list-item-title>
-          <v-list-item-subtitle>{{ benutzerinformationen.lastModifiedDateTime }}</v-list-item-subtitle>
+          <v-list-item-subtitle>{{ letzteAenderung }}</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -51,6 +49,7 @@
 import { Component, VModel, Vue } from "vue-property-decorator";
 import { BearbeitendePersonDto } from "@/api/api-client/isi-backend";
 import _ from "lodash";
+import moment from "moment";
 
 export class BenutzerinformationenModel {
   constructor(bearbeitendePerson?: BearbeitendePersonDto, lastModifiedDateTime?: Date) {
@@ -63,10 +62,10 @@ export class BenutzerinformationenModel {
   public lastModifiedDateTime?: Date;
 }
 
-@Component({
-  components: {},
-})
+@Component
 export default class Benutzerinformationen extends Vue {
+  static readonly DISPLAY_FORMAT = "DD.MM.YYYY";
+
   @VModel({ type: BenutzerinformationenModel })
   benutzerinformationen!: BenutzerinformationenModel;
 
@@ -75,6 +74,49 @@ export default class Benutzerinformationen extends Vue {
       _.isNil(this.benutzerinformationen?.lastModifiedDateTime) ||
       _.isNil(this.benutzerinformationen?.bearbeitendePerson)
     );
+  }
+
+  get name(): string {
+    if (
+      _.isNil(this.benutzerinformationen?.bearbeitendePerson) ||
+      _.isNil(this.benutzerinformationen?.bearbeitendePerson?.name)
+    ) {
+      return "";
+    } else {
+      return this.benutzerinformationen.bearbeitendePerson.name;
+    }
+  }
+
+  get email(): string {
+    if (
+      _.isNil(this.benutzerinformationen?.bearbeitendePerson) ||
+      _.isNil(this.benutzerinformationen?.bearbeitendePerson?.email)
+    ) {
+      return "";
+    } else {
+      return this.benutzerinformationen.bearbeitendePerson.email;
+    }
+  }
+
+  get organisationseinheit(): string {
+    if (
+      _.isNil(this.benutzerinformationen?.bearbeitendePerson) ||
+      _.isNil(this.benutzerinformationen?.bearbeitendePerson?.organisationseinheit)
+    ) {
+      return "";
+    } else {
+      return this.benutzerinformationen.bearbeitendePerson.organisationseinheit;
+    }
+  }
+
+  get letzteAenderung(): string {
+    if (_.isNil(this.benutzerinformationen?.lastModifiedDateTime)) {
+      return "";
+    } else {
+      return moment
+        .utc(this.benutzerinformationen.lastModifiedDateTime, true)
+        .format(Benutzerinformationen.DISPLAY_FORMAT);
+    }
   }
 }
 </script>
