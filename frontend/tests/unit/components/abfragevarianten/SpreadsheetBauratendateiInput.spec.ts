077015d72df213c88e197f11820ffbd296e852e2
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-27 15:46:32 +0100

    add unit test

--- frontend/tests/unit/components/abfragevarianten/SpreadsheetBauratendateiInput.spec.ts
+++ frontend/tests/unit/components/abfragevarianten/SpreadsheetBauratendateiInput.spec.ts
@@ -10,17 +10,17 @@ const bauratendateiInput: Array<WohneinheitenProFoerderartProJahrDto> = [
   },
   {
     jahr: "2024",
-    foerderart: "forderart2",
+    foerderart: "foerderart2",
     wohneinheiten: 11,
   },
   {
     jahr: "2024",
-    foerderart: "forderart3",
+    foerderart: "foerderart3",
     wohneinheiten: 12,
   },
   {
     jahr: "2024",
-    foerderart: "forderart4",
+    foerderart: "foerderart4",
     wohneinheiten: 13,
   },
   {
@@ -30,17 +30,17 @@ const bauratendateiInput: Array<WohneinheitenProFoerderartProJahrDto> = [
   },
   {
     jahr: "2025",
-    foerderart: "forderart2",
+    foerderart: "foerderart2",
     wohneinheiten: 21,
   },
   {
     jahr: "2025",
-    foerderart: "forderart3",
+    foerderart: "foerderart3",
     wohneinheiten: 22,
   },
   {
     jahr: "2025",
-    foerderart: "forderart4",
+    foerderart: "foerderart4",
     wohneinheiten: 23,
   },
   {
@@ -65,21 +65,64 @@ describe("SpreadsheetBauratendateiInput.spec.ts", () => {
         align: "start",
       },
       {
-        text: "forderart2",
-        value: "forderart2",
+        text: "foerderart2",
+        value: "foerderart2",
         align: "start",
       },
       {
-        text: "forderart3",
-        value: "forderart3",
+        text: "foerderart3",
+        value: "foerderart3",
         align: "start",
       },
       {
-        text: "forderart4",
-        value: "forderart4",
+        text: "foerderart4",
+        value: "foerderart4",
         align: "start",
       },
     ];
+    expect(result).toEqual(expected);
+  });
+
+  test("Transformation bauratendateiInput undefined in Header", () => {
+    let result = createHeaders(undefined);
+    let expected: Array<DataTableHeader> = [
+      {
+        align: "start",
+        text: "Jahr",
+        value: "jahr",
+      },
+    ];
+    expect(result).toEqual(expected);
+  });
+
+  test("Transformation bauratendateiInput in Table Data", () => {
+    let result = createTableData(bauratendateiInput);
+    let expected: Array<any> = [
+      {
+        jahr: "2024",
+        foerderart1: 10,
+        foerderart2: 11,
+        foerderart3: 12,
+        foerderart4: 13,
+      },
+      {
+        jahr: "2025",
+        foerderart1: 20,
+        foerderart2: 21,
+        foerderart3: 22,
+        foerderart4: 23,
+      },
+      {
+        jahr: "2026",
+        foerderart1: 30,
+      },
+    ];
+    expect(result).toEqual(expected);
+  });
+
+  test("Transformation bauratendateiInput undefined in Table Data", () => {
+    let result = createTableData(undefined);
+    let expected: Array<any> = [];
     expect(result).toEqual(expected);
   });
 });
