ad220d4c17be7a864723fa7882f2e7c847704a10
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-03-27 09:37:57 +0100

    add example spreadsheet

---
+++ frontend/src/components/abfragevarianten/SpreadsheetBauratendateiInput.vue
@@ -0,0 +1,118 @@
+<template>
+  <v-card
+    class="mx-auto mt-10"
+    max-width="600"
+    outlined
+  >
+    <v-card-title>Vuetify Inline Editor Table </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :search="search"
+      class="elevation-1"
+      fixed-header
+      height="350px"
+    >
+      <v-divider inset></v-divider>
+      <template v-slot:top>
+        <v-toolbar
+          flat
+          color="white"
+        >
+          <div class="d-flex">
+            <v-btn
+              color="primary"
+              class="ml-2"
+              @click="addNew"
+            >
+              <v-icon dark>mdi-plus</v-icon>Add
+            </v-btn>
+          </div>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.name="{ item }">
+        <v-text-field
+          v-model="editedItem.name"
+          :hide-details="true"
+          dense
+          single-line
+          :autofocus="true"
+          v-if="item.id === editedItem.id"
+        ></v-text-field>
+        <span v-else>{{ item.name }}</span>
+      </template>
+      <template v-slot:item.calories="{ item }">
+        <v-text-field
+          v-model="editedItem.calories"
+          :hide-details="true"
+          dense
+          single-line
+          v-if="item.id === editedItem.id"
+        ></v-text-field>
+        <span v-else>{{ item.calories }}</span>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <div v-if="item.id === editedItem.id">
+          <v-icon
+            color="red"
+            class="mr-3"
+            @click="close"
+          >
+            mdi-window-close
+          </v-icon>
+          <v-icon
+            color="green"
+            @click="save"
+          >
+            mdi-content-save
+          </v-icon>
+        </div>
+        <div v-else>
+          <v-icon
+            color="green"
+            class="mr-3"
+            @click="editItem(item)"
+          >
+            mdi-pencil
+          </v-icon>
+          <v-icon
+            color="red"
+            @click="deleteItem(item)"
+          >
+            mdi-delete
+          </v-icon>
+        </div>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="primary"
+          @click="initialize"
+          >Reset</v-btn
+        >
+      </template>
+    </v-data-table>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Mixins, VModel, Prop } from "vue-property-decorator";
+import FieldGroupCard from "@/components/common/FieldGroupCard.vue";
+import NumField from "@/components/common/NumField.vue";
+import SaveLeaveMixin from "@/mixins/SaveLeaveMixin";
+import { DataTableHeader } from "vuetify";
+
+@Component({ components: { FieldGroupCard, NumField } })
+export default class SpreadsheetBauratendateiInput extends Mixins(SaveLeaveMixin) {
+  @VModel({ type: Array })
+  private items!: unknown[];
+
+  @Prop({ type: Array, default: [] })
+  private readonly headers!: DataTableHeader[];
+
+  @Prop({ type: String, default: "spreadsheet" })
+  private readonly id!: string;
+
+  @Prop({ type: Boolean, default: false })
+  private readonly isEditable!: boolean;
+}
+</script>
