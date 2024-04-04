2fc51a5f25edb19390eea4d31249365c52c00015
banzuu <103417158+banzuu@users.noreply.github.com>
2024-04-04 12:38:07 +0200

    Verbessern und Testen der FieldValidationRules

--- frontend/src/utils/FieldValidationRules.ts
+++ frontend/src/utils/FieldValidationRules.ts
@@ -4,39 +4,38 @@ import FoerdermixModel from "@/types/model/bauraten/FoerdermixModel";
 import { addiereAnteile } from "@/utils/CalculationUtil";
 import { UncertainBoolean } from "@/api/api-client/isi-backend";
 
-export function pflichtfeld(v: string): boolean | string {
+type Nil = undefined | null;
+type Result = true | string;
+
+export function pflichtfeld(v: string | Nil): Result {
   return !!v || "Pflichtfeld";
 }
 
-export function pflichtfeldMehrfachauswahl(v: Array<string>): boolean | string {
+export function pflichtfeldMehrfachauswahl(v: unknown[] | Nil): Result {
   return !!(v && v.length) || "Pflichtfeld";
 }
 
-export function pflichtfeldNotNil(v: string): boolean | string {
-  return !_.isNil(v) || "Pflichtfeld";
-}
-
-export function buchstaben(v: string): boolean | string {
+export function buchstaben(v: string | Nil): Result {
   return !v || /^[a-zA-Z_äÄöÖüÜß -\s]*$/.test(v) || "Nur Buchstaben erlaubt";
 }
 
-export function min5(v: string): boolean | string {
+export function min5(v: string | Nil): Result {
   return !v || /.{5,}$/.test(v) || "Mindestens fünf Zeichen benötigt";
 }
 
-export function hausnummer(v: string): boolean | string {
+export function hausnummer(v: string | Nil): Result {
   return !v || /^[a-zA-Z 0-9 \s]*$/.test(v) || "Nur Buchstaben und Zahlen erlaubt";
 }
 
-export function datum(format: string): (v: string) => boolean | string {
-  return (v: string) => !v || moment(v, format, true).isValid() || "Muss korrekt formatiert sein";
+export function datum(format: string): (v: string | Nil) => Result {
+  return (v: string | Nil) => !v || moment(v, format, true).isValid() || "Muss korrekt formatiert sein";
 }
 
-export function digits(v: string): boolean | string {
+export function digits(v: string | Nil): Result {
   return !v || /^\d*$/.test(v) || "Nur Ziffern erlaubt";
 }
 
-export function notUnspecified(v: string): boolean | string {
+export function notUnspecified(v: string | Nil): Result {
   return (!_.isNil(v) && v !== UncertainBoolean.Unspecified) || "Pflichtfeld";
 }
 
@@ -45,13 +44,13 @@ export function notUnspecified(v: string): boolean | string {
  * Evtl. vorhandene Formatierungen werden entfernt (Tausender-Trennzeichen) oder
  * umgewandelt (Komma in Punkt).
  */
-export function min(limit: number): (v: string) => boolean | string {
-  return (v: string) => {
+export function min(limit: number): (v: string | Nil) => Result {
+  return (v: string | Nil) => {
     if (!_.isNil(v) && !_.isNaN(v)) {
       v = _.replace(v, /\./g, "");
       v = _.replace(v, ",", ".");
     }
-    const num = parseFloat(v);
+    const num = parseFloat(v ?? "");
     return _.isNaN(num) || num >= limit || `Darf nicht kleiner als ${limit} sein`;
   };
 }
@@ -61,13 +60,13 @@ export function min(limit: number): (v: string) => boolean | string {
  * Evtl. vorhandene Formatierungen werden entfernt (Tausender-Trennzeichen) oder
  * umgewandelt (Komma in Punkt). 999.999.999,99 -> 99999999.99
  */
-export function max(limit: number): (v: string) => boolean | string {
-  return (v: string) => {
+export function max(limit: number): (v: string | Nil) => Result {
+  return (v: string | Nil) => {
     if (!_.isNil(v) && !_.isNaN(v)) {
       v = _.replace(v, /\./g, "");
       v = _.replace(v, ",", ".");
     }
-    const num = parseFloat(v);
+    const num = parseFloat(v ?? "");
     return _.isNaN(num) || num <= limit || `Darf nicht größer als ${limit} sein`;
   };
 }
@@ -76,7 +75,7 @@ export function max(limit: number): (v: string) => boolean | string {
  * Rule die Überprüft ob der Gesamtwert nicht über 100 Prozent liegt.
  * Werte die null/undefined sind werden als 0 gewertet
  */
-export function nichtGleich100Prozent(foerdermix: FoerdermixModel): boolean | string {
+export function nichtGleich100Prozent(foerdermix: FoerdermixModel | Nil): Result {
   if (!_.isNil(foerdermix)) {
     const sum: number = addiereAnteile(foerdermix);
     if (sum < 100) {
@@ -91,7 +90,7 @@ export function nichtGleich100Prozent(foerdermix: FoerdermixModel): boolean | st
       return true;
     }
   } else {
-    return false;
+    return "Es kann nicht summiert werden";
   }
   return true;
 }
