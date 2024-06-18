d9824d57948891cfcf8a25541d01bd82eaa29860
alexander.boxhorn <alexander.boxhorn@muenchen.de>
2024-06-18 10:38:39 +0200

    add composable for document security

---
+++ frontend/src/composables/security/DocumentSecurity.ts
@@ -0,0 +1,41 @@
+import { useSecurity } from "@/composables/security/Security";
+import { Context } from "@/utils/Context";
+
+export function useDocumentSecurity() {
+  const security = useSecurity();
+
+  function areDokumenteVisible(context: Context): boolean {
+    switch (context) {
+      case Context.ABFRAGE:
+        return security.isRoleAdminOrAbfrageerstellung.value || security.isRoleAdminOrSachbearbeitung.value;
+      case Context.ABFRAGEVARIANTE_SACHBEARBEITUNG:
+        return security.isRoleAdminOrSachbearbeitung.value || security.isRoleAdminOrBedarfsmeldung.value;
+      case Context.BAUVORHABEN:
+        return (
+          security.isRoleAdminOrAbfrageerstellung.value ||
+          security.isRoleAdminOrSachbearbeitung.value ||
+          security.isRoleAdminOrBedarfsmeldung.value
+        );
+      case Context.INFRASTRUKTUREINRICHTUNG:
+        return security.isRoleAdminOrSachbearbeitung.value || security.isRoleAdminOrBedarfsmeldung.value;
+      default:
+        return false;
+    }
+  }
+
+  function areKommentareVisible(context: Context): boolean {
+    switch (context) {
+      case Context.BAUVORHABEN:
+        return security.isRoleAdminOrSachbearbeitung.value;
+      case Context.INFRASTRUKTUREINRICHTUNG:
+        return security.isRoleAdminOrSachbearbeitung.value || security.isRoleAdminOrBedarfsmeldung.value;
+      default:
+        return false;
+    }
+  }
+
+  return {
+    areDokumenteVisible,
+    areKommentareVisible,
+  };
+}
