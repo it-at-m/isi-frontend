abe8da790f2dcb0622f45c4429d5a2deb496849c
TiagoKocevar <tiago.kocevar@gmail.com>
2026-01-29 14:52:07 +0100

    Versorgungsquote Auswahlfeld added :lipstick:️

--- src/api/api-client/isi-geodata-eai/apis/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './ControllerFrGeometriespezifischeOperationenImStandardEPSG4326WGS84Api';
-export * from './ControllerZurExtraktionVonFeatureAufBasisVonMultiPolygoneImStandardEPSG4326WGS84Api';
-export * from './ControllerZurExtraktionVonFeatureAufBasisVonPunktKoordinatenImStandardEPSG4326WGS84Api';
