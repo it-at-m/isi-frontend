c1e0b18b876c0c384a6d3e471523ca769242c689
albrecht.schaenzel <albrecht.schaenzel@muenchen.de>
2026-02-16 16:17:41 +0100

    Work saved

--- src/utils/MapUtil.ts
+++ src/utils/MapUtil.ts
@@ -51,9 +51,9 @@ export const OVERLAYS_ARCGIS_TRANSPARENT: OverlayUrlMapping[] = [
   { displayName: "Grundschulsprengel", internalName: "Grundschulsprengel", urlPart: "Bildung_und_Soziales" },
   { displayName: "Mittelschulsprengel", internalName: "Mittelschulsprengel", urlPart: "Bildung_und_Soziales" },
   { displayName: "Baublöcke", internalName: "Baublöcke", urlPart: "basis" },
-  { displayName: "Bebauungspläne rechtskräftig", internalName: "BAUG_UMGRIFF", urlPart: "basis" },
-  { displayName: "Bebauungspläne in Aufstellung", internalName: "BAUG_UMGRIFF", urlPart: "basis" },
-  { displayName: "Bauliniengefüge", internalName: "BAUG_LINIE", urlPart: "basis" },
+  { displayName: "Bebauungspläne rechtskräftig", internalName: "BB_rechtskräftig", urlPart: "basis" },
+  { displayName: "Bebauungspläne in Aufstellung", internalName: "BB-Umgriff_in_Aufstellung", urlPart: "basis" },
+  { displayName: "Bauliniengefüge", internalName: "Bauliniengefüge ", urlPart: "basis" },
 ];
 
 /**
