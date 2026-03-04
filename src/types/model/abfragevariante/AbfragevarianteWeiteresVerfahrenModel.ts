1aab880d48aaecb66890c7b6c404e6f909a39791
lhmAschaenz <119875041+lhmAschaenz@users.noreply.github.com>
2026-03-04 09:35:03 +0100

    ISI-1850-Whitescreen-bei-Datenuebernahme-und-Loeschen-von-Varianten (#403)
    * Bugfix und Refactoring
    
    * applied coderabbitai recommendations

--- src/types/model/abfragevariante/AbfragevarianteWeiteresVerfahrenModel.ts
+++ src/types/model/abfragevariante/AbfragevarianteWeiteresVerfahrenModel.ts
@@ -1,5 +1,4 @@
 import type { AbfragevarianteWeiteresVerfahrenDto } from "@/api/api-client/isi-backend";
-import { AnzeigeContextAbfragevariante } from "@/types/common/Abfrage";
 import BauabschnittModel from "@/types/model/bauabschnitte/BauabschnittModel";
 import _ from "lodash";
 import SobonBerechnungModel from "@/types/model/abfragevariante/SobonBerechnungModel";
@@ -18,18 +17,6 @@ class AbfragevarianteWeiteresVerfahrenModel {
     } else {
       this.sobonBerechnung = createSobonBerechnungWeiteresVerfahren();
     }
-  }
-
-  public getAbfragevariantenNrForContextAnzeigeAbfragevariante(
-    contextAnzeigeAbfragevariante: AnzeigeContextAbfragevariante,
-  ): string {
-    let numberContext = "";
-    if (contextAnzeigeAbfragevariante === AnzeigeContextAbfragevariante.ABFRAGEVARIANTE) {
-      numberContext = `1.${this.abfragevariantenNr}`;
-    } else if (contextAnzeigeAbfragevariante === AnzeigeContextAbfragevariante.ABFRAGEVARIANTE_SACHBEARBEITUNG) {
-      numberContext = `2.${this.abfragevariantenNr}`;
-    }
-    return numberContext;
   }
 }
 export { AbfragevarianteWeiteresVerfahrenModel as default };
---
+++ src/utils/AbfragevarianteUtil.ts
@@ -0,0 +1,14 @@
+import { AnzeigeContextAbfragevariante, type AnyAbfragevarianteModel } from "@/types/common/Abfrage";
+
+export function getAbfragevariantenNrForContextAnzeigeAbfragevariante(
+  abfragevarianteModel: AnyAbfragevarianteModel,
+  contextAnzeigeAbfragevariante: AnzeigeContextAbfragevariante,
+): string {
+  let numberContext = "";
+  if (contextAnzeigeAbfragevariante === AnzeigeContextAbfragevariante.ABFRAGEVARIANTE) {
+    numberContext = `1.${abfragevarianteModel.abfragevariantenNr}`;
+  } else if (contextAnzeigeAbfragevariante === AnzeigeContextAbfragevariante.ABFRAGEVARIANTE_SACHBEARBEITUNG) {
+    numberContext = `2.${abfragevarianteModel.abfragevariantenNr}`;
+  }
+  return numberContext;
+}
