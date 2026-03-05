f8505fa7fd9a83003dcc7f50e0a03eb0ec9e4f8b
TiagoKocevar <tiago.kocevar@gmail.com>
2026-03-05 10:27:21 +0100

    Type fix for serialization

--- src/utils/SerializationUtils.ts
+++ src/utils/SerializationUtils.ts
@@ -1,9 +1,11 @@
-export function toBackendJson<T>(value: T): T {
+export type BackendJson = string | number | boolean | null | BackendJson[] | { [k: string]: BackendJson };
+
+export function toBackendJson<T>(value: T): BackendJson {
   return JSON.parse(
     JSON.stringify(value, (_k, v) => {
       if (v instanceof Set) return Array.from(v);
       if (v instanceof Map) return Object.fromEntries(v);
       return v;
     }),
-  );
+  ) as BackendJson;
 }
