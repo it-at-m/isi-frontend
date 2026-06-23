623a5d3b9845a80c32658e66a88b43600541c179
TiagoKocevar <tiago.kocevar@gmail.com>
2026-06-23 14:56:44 +0200

    Fix Layout

--- src/components/abfragevarianten/SachbearbeitungComponent.vue
+++ src/components/abfragevarianten/SachbearbeitungComponent.vue
@@ -26,28 +26,24 @@
           </v-slide-y-reverse-transition>
         </v-col>
         <v-col
+          v-if="isBauleitplanverfahren"
           cols="12"
           md="6"
         >
-          <v-slide-y-reverse-transition>
-            <v-select
-              v-if="
-                isBauleitplanverfahrenOrWeiteresVerfahren &&
-                abfragevarianteSachbearbeitung.sobonBerechnung?.isASobonBerechnung
-              "
-              id="sobon_orientierungswert_jahr_sobonursaechlich_dropdown"
-              ref="sobonOrientierungswertJahrSobonursaechlichDropdown"
-              v-model="abfragevarianteSachbearbeitung.sobonBerechnung.sobonOrientierungswertJahrSobonUrsaechlich"
-              variant="underlined"
-              :disabled="!isEditableBySachbearbeitung"
-              :items="sobonOrientierungswertJahrList"
-              item-value="key"
-              item-title="value"
-              @update:model-value="formChanged"
-            >
-              <template #label> Jahr für SoBoN-Orientierungwerte (SoBoN-ursächlich) </template>
-            </v-select>
-          </v-slide-y-reverse-transition>
+          <v-select
+            id="bauratenmethodik_dropdown"
+            ref="bauratenmethodikDropdown"
+            v-model="abfragevarianteSachbearbeitung.sobonBerechnung.bauratenmethodik"
+            variant="underlined"
+            :disabled="!isEditableBySachbearbeitung"
+            :items="lookupStore.bauratenmethodik"
+            item-value="key"
+            item-title="value"
+            :rules="bauratenmethodikValidator"
+            @update:model-value="formChanged"
+          >
+            <template #label> Bauratenmethodik <span class="text-secondary">*</span> </template>
+          </v-select>
         </v-col>
         <!-- Das Datum wird in ISI 2.0 relevant werden
         <v-col
@@ -69,32 +65,8 @@
       <sobon-berechnung
         v-if="isBauleitplanverfahrenOrWeiteresVerfahren"
         v-model="abfragevarianteSachbearbeitung.sobonBerechnung"
+        :sobon-orientierungswert-jahr-list="sobonOrientierungswertJahrList"
       ></sobon-berechnung>
-      <v-row v-if="isBauleitplanverfahren">
-        <v-col
-          cols="12"
-          md="6"
-        />
-        <v-col
-          cols="12"
-          md="6"
-        >
-          <v-select
-            id="bauratenmethodik_dropdown"
-            ref="bauratenmethodikDropdown"
-            v-model="abfragevarianteSachbearbeitung.sobonBerechnung.bauratenmethodik"
-            variant="underlined"
-            :disabled="!isEditableBySachbearbeitung"
-            :items="lookupStore.bauratenmethodik"
-            item-value="key"
-            item-title="value"
-            :rules="bauratenmethodikValidator"
-            @update:model-value="formChanged"
-          >
-            <template #label> Bauratenmethodik <span class="text-secondary">*</span> </template>
-          </v-select>
-        </v-col>
-      </v-row>
       <v-row>
         <v-col
           cols="12"
